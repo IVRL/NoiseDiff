c661c298f1273360eb83ffda6fe82e9e2ce42d67
SkyeLu <31540747+SkyeLu@users.noreply.github.com>
2025-07-01 11:27:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
-pip install tensorboardX
-pip install rawpy
-pip install exifread
-pip install ema-pytorch
-pip install denoising_diffusion_pytorch
-pip install scikit-image
-pip install -U numba
-pip install lpips
-pip install yacs
+#!/bin/bash
+
+pip install torch==2.0.1+cu117 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torchvision==0.15.2+cu117 -f https://download.pytorch.org/whl/torch_stable.html
+
+pip install \
+  tensorboardX \
+  rawpy \
+  matplotlib \
+  opencv-python \
+  exifread \
+  ema-pytorch \
+  denoising_diffusion_pytorch \
+  scikit-image \
+  -U numba \
+  lpips \
+  yacs \
+  scikit-learn
